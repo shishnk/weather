eac1ec54cbd6b29581bbf41f754c48b03d0c969c
Hukutka <akorund@mail.ru>
2023-05-23 22:06:48 +0700

    Add project files.

---
+++ src/weather/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace weather.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
---
+++ tests/Weather.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Weather.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
